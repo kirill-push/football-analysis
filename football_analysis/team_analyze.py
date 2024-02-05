ce7eb96e06ebd269b65c946d6f768ed7a82d5d13
kirill-push <kirillpush@gmail.com>
2024-02-05 14:22:41 +0200

    feat: add func to calculate average color of bbox

--- football_analysis/team_analyze.py
+++ football_analysis/team_analyze.py
@@ -53,3 +53,21 @@ def remove_background_and_get_avg_color(
     avg_color = np.mean(filtered_img, axis=(0))
 
     return masked_img, avg_color
+
+
+def get_average_color(image: np.ndarray, box: List) -> np.ndarray:
+    """Calculates average color of a region within an image specified by bounding box.
+
+    Args:
+        image (np.ndarray): The input image.
+        box (List): A list containing the coordinates of the bounding box in format
+            (x1, y1, x2, y2, _).
+
+    Returns:
+        np.ndarray: The average color of the specified region.
+    """
+    crop_img = crop_image(image, box, head=0.2, legs=0.5)
+    avg_color_per_row = np.average(crop_img, axis=0)
+    avg_color = np.average(avg_color_per_row, axis=0)
+
+    return avg_color
